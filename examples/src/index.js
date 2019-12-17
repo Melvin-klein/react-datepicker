715445c04a9cba15609fda9aad3274a1c62661f1
Melvin <m.nargeot@archidvisor.com>
2019-12-17 22:12:04 +0100

    Create index.js

---
+++ examples/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Datepicker from "../../src";
+
+const App = () => (
+    <Datepicker />
+);
+
+render(<App />, document.getElementById("root"));
